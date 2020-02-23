e7a147ab4064933c9587a8eaa45e523e72f536f2
Sean Lyo <137232774@qq.com>
2020-02-24 00:20:39 +0800

    crate `serde_json` add serialize BTreeMap to json

--- crate_serde_json_serialize_hashmap/src/main.rs
+++ crate_serde_json_serialize_hashmap/src/main.rs
@@ -1,9 +1,12 @@
 extern crate serde_json;
 
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 
 fn main() {
     let mut s = HashMap::new();
+    let mut t = BTreeMap::new();
     s.insert("number", 1);
+    t.insert("num", 1);
     println!("{}", serde_json::to_string_pretty(&s).unwrap());
+    println!("{}", serde_json::to_string_pretty(&t).unwrap());
 }
