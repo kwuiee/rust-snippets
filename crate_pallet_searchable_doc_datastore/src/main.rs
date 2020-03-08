b6327eb6dd50ad57090a3607e1c2b88991d0298f
Sean Lyo <137232774@qq.com>
2020-03-08 20:05:48 +0800

    add crate `pallet` searchable doc datastore

---
+++ crate_pallet_searchable_doc_datastore/src/main.rs
@@ -0,0 +1,53 @@
+#[macro_use]
+extern crate serde;
+extern crate pallet;
+extern crate sled;
+extern crate tempfile;
+
+#[derive(Serialize, Deserialize, Debug, pallet::DocumentLike)]
+#[pallet(tree_name = "books")]
+pub struct Book {
+    #[pallet(default_search_field)]
+    title: String,
+    #[pallet(default_search_field)]
+    description: Option<String>,
+    #[pallet(index_field_type = "u64")]
+    rating: u8,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let temp_dir = tempfile::TempDir::new_in(".")?;
+
+    let db = sled::open(temp_dir.path().join("db"))?;
+
+    let store = pallet::Store::builder()
+        .with_db(db)
+        .with_index_dir(temp_dir.path())
+        .finish()?;
+
+    let books = vec![
+        Book {
+            title: "The Old Man and the Sea".into(),
+            description: Some(
+                "He was an old man who fished alone in a skiff in \
+            the Gulf Stream and he had gone eighty-four days \
+            now without taking a fish."
+                    .into(),
+            ),
+            rating: 10,
+        },
+        Book {
+            title: "The Great Gatsby".into(),
+            description: Some("About a man and some other stuff".into()),
+            rating: 8,
+        },
+    ];
+
+    let _ = store.create_multi(&books)?;
+
+    let books = store.search("man AND rating:>8")?;
+
+    println!("{:?}", books);
+
+    Ok(())
+}
