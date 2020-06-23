c6a92c338834c135beffea0f933c3c2425a5810f
slyo <sean.lyo@outlook.com>
2020-06-23 23:25:17 +0800

    feature(ncurses-rs): crate `ncurse-rs`(rust bindings for c ncurses) tui library examples

---
+++ crate_ncurses-rs_tui_library_examples/src/bin/basic_input.rs
@@ -0,0 +1,49 @@
+/*
+    Copyright © 2013 Free Software Foundation, Inc
+    See licensing in LICENSE file
+    File: examples/ex_2.rs
+    Author: Jesse 'Jeaye' Wilkerson
+    Description:
+      Basic input and attribute example.
+*/
+
+extern crate ncurses;
+
+use ncurses::*;
+use std::char;
+
+fn main() {
+    /* Setup ncurses. */
+    initscr();
+    raw();
+
+    /* Allow for extended keyboard (like F1). */
+    keypad(stdscr(), true);
+    noecho();
+
+    /* Prompt for a character. */
+    addstr("Enter a character: ");
+
+    /* Wait for input. */
+    let ch = getch();
+    if ch == KEY_F1 {
+        /* Enable attributes and output message. */
+        attron(A_BOLD() | A_BLINK());
+        addstr("\nF1");
+        attroff(A_BOLD() | A_BLINK());
+        addstr(" pressed");
+    } else {
+        /* Enable attributes and output message. */
+        addstr("\nKey pressed: ");
+        attron(A_BOLD() | A_BLINK());
+        addstr(format!("{}\n", char::from_u32(ch as u32).expect("Invalid char")).as_ref());
+        attroff(A_BOLD() | A_BLINK());
+    }
+
+    /* Refresh, showing the previous message. */
+    refresh();
+
+    /* Wait for one more character before exiting. */
+    getch();
+    endwin();
+}
---
+++ crate_ncurses-rs_tui_library_examples/src/bin/basic_input_unicode.rs
@@ -0,0 +1,83 @@
+/*
+    Copyright © 2013 Free Software Foundation, Inc
+    See licensing in LICENSE file
+    File: examples/ex_7.rs
+    Author: Jesse 'Jeaye' Wilkerson
+    Description:
+      Basic input and attribute example, using the Unicode-aware get_wch functions.
+*/
+
+extern crate ncurses;
+
+use ncurses::*;
+use std::char;
+
+#[cfg(feature = "wide")]
+fn main() {
+    let locale_conf = LcCategory::all;
+    setlocale(locale_conf, "en_US.UTF-8");
+
+    /* Setup ncurses. */
+    initscr();
+    raw();
+
+    /* Require input within 2 seconds. */
+    halfdelay(20);
+    /* Enable mouse events. */
+    mousemask(ALL_MOUSE_EVENTS as mmask_t, None);
+
+    /* Allow for extended keyboard (like F1). */
+    keypad(stdscr(), true);
+    noecho();
+
+    /* Prompt for a character. */
+    addstr("Enter a character within 2 seconds: ");
+
+    /* Wait for input. */
+    let ch = wget_wch(stdscr());
+    match ch {
+        Some(WchResult::KeyCode(KEY_MOUSE)) => {
+            /* Enable attributes and output message. */
+            attron(A_BOLD() | A_BLINK());
+            addstr("\nMouse");
+            attroff(A_BOLD() | A_BLINK());
+            addstr(" pressed");
+        }
+
+        Some(WchResult::KeyCode(_)) => {
+            /* Enable attributes and output message. */
+            attron(A_BOLD() | A_BLINK());
+            addstr("\nKeycode");
+            attroff(A_BOLD() | A_BLINK());
+            addstr(" pressed");
+        }
+
+        Some(WchResult::Char(c)) => {
+            /* Enable attributes and output message. */
+            addstr("\nKey pressed: ");
+            attron(A_BOLD() | A_BLINK());
+            addstr(format!("{}\n", char::from_u32(c as u32).expect("Invalid char")).as_ref());
+            attroff(A_BOLD() | A_BLINK());
+        }
+
+        None => {
+            addstr("\nYou didn't enter a character in time!");
+        }
+    }
+
+    /* Refresh, showing the previous message. */
+    refresh();
+
+    /* Wait for one more character before exiting. Disable the input timeout. */
+    nocbreak();
+    getch();
+    endwin();
+}
+
+#[cfg(not(feature = "wide"))]
+fn main() {
+    initscr();
+    addstr("This example requires wide character support.");
+    getch();
+    endwin();
+}
