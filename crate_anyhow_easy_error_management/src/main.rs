73735c574c53b9fdd432b058256ce60c26fa8c70
slyo <sean.lyo@outlook.com>
2020-07-05 16:48:00 +0800

    feature(anyhow): add threading error handle

--- crate_anyhow_easy_error_management/src/main.rs
+++ crate_anyhow_easy_error_management/src/main.rs
@@ -8,6 +8,8 @@ use anyhow::Result;
 use std::fs::File;
 use std::io::Read;
 use std::str;
+use std::sync::mpsc::channel;
+use std::thread;
 use toml::value::Value;
 
 #[derive(Deserialize, Serialize)]
@@ -17,18 +19,33 @@ struct Config {
 }
 
 fn main() -> Result<()> {
-    // deserialze as toml::value::Value
     let mut content = vec![];
     File::open("Cargo.toml")?.read_to_end(&mut content)?;
     let value: Value = toml::from_slice(&content)?;
     println!("{}\n", value["dependencies"]["serde"]);
-
-    // deserialize as local defined type
     let val: Config = toml::from_str(&"username = 'Sean Lyo'\nid = 1234")?;
     println!("{}\n", val.username);
-
-    // serialize to string
     let ser: String = toml::to_string_pretty(&val)?;
     println!("{}\n", ser);
+
+    // threading error handling
+    let (tx, rx) = channel();
+    let sender = thread::spawn(move || -> Result<u8> {
+        tx.send("Hello, thread".to_owned())?;
+        Ok(0)
+    });
+    let receiver = thread::spawn(move || -> Result<u8> {
+        let value = rx.recv()?;
+        println!("{}", value);
+        Ok(0)
+    });
+    match sender.join() {
+        Ok(t) => println!("return value of send: {}", t?),
+        Err(err) => println!("error returning send value: {:?}", err),
+    };
+    match receiver.join() {
+        Ok(t) => println!("return value of send: {}", t?),
+        Err(err) => println!("error returning send value: {:?}", err),
+    };
     Ok(())
 }
