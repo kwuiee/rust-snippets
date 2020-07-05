ea695bbdcd5535dea51f6ee7d1dbf7b2c64eb4a8
slyo <sean.lyo@outlook.com>
2020-07-05 15:49:27 +0800

    feature(anyhow): error handling with ease

---
+++ crate_anyhow_easy_error_management/src/main.rs
@@ -0,0 +1,34 @@
+extern crate anyhow;
+extern crate serde;
+extern crate toml;
+
+use serde::{Deserialize, Serialize};
+
+use anyhow::Result;
+use std::fs::File;
+use std::io::Read;
+use std::str;
+use toml::value::Value;
+
+#[derive(Deserialize, Serialize)]
+struct Config {
+    username: String,
+    id: u16,
+}
+
+fn main() -> Result<()> {
+    // deserialze as toml::value::Value
+    let mut content = vec![];
+    File::open("Cargo.toml")?.read_to_end(&mut content)?;
+    let value: Value = toml::from_slice(&content)?;
+    println!("{}\n", value["dependencies"]["serde"]);
+
+    // deserialize as local defined type
+    let val: Config = toml::from_str(&"username = 'Sean Lyo'\nid = 1234")?;
+    println!("{}\n", val.username);
+
+    // serialize to string
+    let ser: String = toml::to_string_pretty(&val)?;
+    println!("{}\n", ser);
+    Ok(())
+}
