e759ae2398333047477d7e8bfdadfca59522f0f9
slyo <sean.lyo@outlook.com>
2020-07-20 15:54:06 +0800

    fix(lettre): fix bug

--- crate_lettre_send_email/src/main.rs
+++ crate_lettre_send_email/src/main.rs
@@ -1,27 +1,26 @@
 extern crate lettre;
 extern crate lettre_email;
-extern crate mime;
 
 use lettre::smtp::authentication::Credentials;
 use lettre::{SmtpClient, Transport};
 use lettre_email::Email;
 
 fn main() {
-    let email_receiver = "liuxiaochuan@novogene.com";
-    let mine_email = "liuxiaochuan@novogene.com";
+    let email_receiver = "receiver@domain.com";
+    let sender = "sender@domain.com";
     let smtp_server = "smtp.exmail.qq.com";
-    let password = "Wfaj668kgYaYW7Xe"; //需要生成应用专用密码
+    let password = "password"; //需要生成应用专用密码
 
     let email = Email::builder()
         .to(email_receiver)
-        .from(mine_email)
+        .from(sender)
         .subject("subject")
         .html("<h1>Hi there</h1>")
         .text("Message send by lettre Rust")
         .build()
         .unwrap();
 
-    let creds = Credentials::new(mine_email.to_string(), password.to_string());
+    let creds = Credentials::new(sender.to_string(), password.to_string());
 
     // Open connection to Gmail
     let mut mailer = SmtpClient::new_simple(smtp_server)
