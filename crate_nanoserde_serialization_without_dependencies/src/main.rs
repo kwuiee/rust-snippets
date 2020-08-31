1d7f606b9909a49d96a7e112688d59ad095dcf4e
slyo <sean.lyo@outlook.com>
2020-08-31 16:17:58 +0800

    feature(nanoserde): serialization without dependencies, monomorphisation

---
+++ crate_nanoserde_serialization_without_dependencies/src/main.rs
@@ -0,0 +1,31 @@
+use nanoserde::{DeBin, DeJson, SerBin, SerJson};
+
+fn main() {
+    #[derive(DeBin, SerBin, DeJson, SerJson, PartialEq)]
+    pub struct Test {
+        pub a: i32,
+        pub b: f32,
+        c: Option<String>,
+        d: Option<String>,
+        e: Option<std::collections::HashMap<String, String>>,
+    }
+
+    let mut map = std::collections::HashMap::new();
+    map.insert("a".to_string(), "b".to_string());
+
+    let test: Test = Test {
+        a: 1,
+        b: 2.,
+        c: Some("asd".to_string()),
+        d: None,
+        e: Some(map),
+    };
+
+    let bytes = SerBin::serialize_bin(&test);
+    let test_deserialized = DeBin::deserialize_bin(&bytes).unwrap();
+    assert!(test == test_deserialized);
+
+    let bytes = SerJson::serialize_json(&test);
+    let test_deserialized = DeJson::deserialize_json(&bytes).unwrap();
+    assert!(test == test_deserialized);
+}
