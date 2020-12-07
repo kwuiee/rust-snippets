341dc4cd206eb811644e32c2ad6fd395cda55c82
slyo <sean.lyo@outlook.com>
2020-12-07 20:43:32 +0800

    feature(bam): Bam read examples merge.

--- crate_bam_read_bam/src/main.rs
+++ crate_bam_read_bam/src/main.rs
@@ -1,23 +1,40 @@
 extern crate bam;
 
 // You need to import RecordReader trait
-use bam::RecordReader;
-use std::env;
+use bam::index::Index;
+use bam::{IndexedReader, Pileup, RecordReader, Region};
 
 fn main() {
-    let mut reader = bam::BamReader::from_path(env::args().nth(1).unwrap(), 4).unwrap();
+    // Read per record
+    let mut reader = bam::BamReader::from_path("tests/sample.bam", 0).unwrap();
     let mut record = bam::Record::new();
-    let mut corru = 1;
+    let mut corr = 0;
     loop {
         match reader.read_into(&mut record) {
-            Ok(()) => {}
-            Err(bam::Error::NoMoreRecords) => break,
+            Ok(true) => {}
+            Ok(false) => break,
             Err(e) => {
-                corru += 1;
+                corr += 1;
                 println!("{}", e)
             }
         }
         // Do something.
     }
-    println!("corrupted number: {}", corru)
+    println!("Corrupted number {}", corr);
+    // Read index
+    let index = Index::from_path("tests/sample.bam.bai").unwrap();
+    for i in index.fetch_chunks(0, 127693, 386189) {
+        println!("{}", i);
+    }
+    // Indexed pileup
+    let mut ireader = IndexedReader::from_path("tests/sample.bam").unwrap();
+    for pc in Pileup::new(&mut ireader.fetch(&Region::new(0, 127693, 386189)).unwrap()) {
+        let pc = pc.unwrap();
+        println!(
+            "ref {} position {} depth {}",
+            pc.ref_id(),
+            pc.ref_pos(),
+            pc.entries().len()
+        )
+    }
 }
--- crate_bam_read_bam/tests/run.sh
+++
@@ -1,1 +0,0 @@
-time ../target/release/crate_bam_read_bam /TJPROJ4/BioAI/liuxiaochuan/langs/Rust/dev/bsum/data/Sample_A.dedup.bam
