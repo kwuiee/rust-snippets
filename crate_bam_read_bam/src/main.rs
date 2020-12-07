42fa9b18a802f149397211888abb7bd13e827329
slyo <sean.lyo@outlook.com>
2020-12-07 22:06:38 +0800

    fix: Fix `bam` examples

--- crate_bam_read_bam/src/main.rs
+++ crate_bam_read_bam/src/main.rs
@@ -11,16 +11,23 @@ fn main() {
     let mut corr = 0;
     loop {
         match reader.read_into(&mut record) {
-            Ok(true) => {}
+            Ok(true) => {
+                // Do something
+            }
             Ok(false) => break,
             Err(e) => {
                 corr += 1;
                 println!("{}", e)
             }
         }
-        // Do something.
     }
     println!("Corrupted number {}", corr);
+    // Iterate over alignment entry
+    let mut reader = bam::BamReader::from_path("tests/sample.bam", 0).unwrap();
+    let record = reader.nth(1).unwrap().unwrap();
+    for i in record.alignment_entries().unwrap() {
+        println!("Record position {:?}; reference position {:?}; record nuclide {:?}; reference nuclide {:?}", i.record_pos(), i.ref_pos(), i.record_nt(), i.ref_nt());
+    }
     // Read index
     let index = Index::from_path("tests/sample.bam.bai").unwrap();
     for i in index.fetch_chunks(0, 127693, 386189) {
@@ -28,7 +35,7 @@ fn main() {
     }
     // Indexed pileup
     let mut ireader = IndexedReader::from_path("tests/sample.bam").unwrap();
-    for pc in Pileup::new(&mut ireader.fetch(&Region::new(0, 127693, 386189)).unwrap()) {
+    for pc in Pileup::new(&mut ireader.fetch(&Region::new(0, 127693, 127700)).unwrap()) {
         let pc = pc.unwrap();
         println!(
             "ref {} position {} depth {}",
