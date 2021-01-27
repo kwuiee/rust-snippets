353f04ab576c3faebe12e848de332628fd5f6b37
slyo <sean.lyo@outlook.com>
2021-01-27 10:28:34 +0800

    feature(num-bigint): Numeric big int.

---
+++ crate_num-bigint_numeric_big_int/src/main.rs
@@ -0,0 +1,31 @@
+use num_bigint::{BigUint, RandBigInt, ToBigInt};
+use num_traits::{One, Zero};
+use std::mem;
+
+// Calculate large fibonacci numbers.
+fn fib(n: usize) -> BigUint {
+    let mut f0: BigUint = Zero::zero();
+    let mut f1: BigUint = One::one();
+    for _ in 0..n {
+        let f2 = f0 + &f1;
+        // This is a low cost way of swapping f0 with f1 and f1 with f2.
+        f0 = mem::replace(&mut f1, f2);
+    }
+    f0
+}
+
+fn main() {
+    // This is a very large number.
+    println!("fib(1000) = {}", fib(1000));
+
+    // Generate large random numbers.
+    let mut rng = rand::thread_rng();
+    let a = rng.gen_bigint(1000);
+
+    let low = -10000.to_bigint().unwrap();
+    let high = 10000.to_bigint().unwrap();
+    let b = rng.gen_bigint_range(&low, &high);
+
+    // Probably an even larger number.
+    println!("{}", a * b);
+}
