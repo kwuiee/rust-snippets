5825e7506bfb0f087b0104a91265acfddfe88798
slyo <sean.lyo@outlook.com>
2021-03-03 22:40:14 +0800

    feature(rayon): Parallel port scan with rayon.

---
+++ crate_rayon_scan_port/src/main.rs
@@ -0,0 +1,18 @@
+use std::net::{TcpStream, ToSocketAddrs};
+
+use rayon::prelude::*;
+
+fn main() {
+    let mut addrs = "rust-lang.org:0".to_socket_addrs().unwrap();
+    let base_addr = addrs.next().unwrap();
+    (0..=65535_u16).into_par_iter().for_each(|i| {
+        let mut addr = base_addr.clone();
+        addr.set_port(i);
+        let mark = if TcpStream::connect(addr).is_ok() {
+            "open"
+        } else {
+            "closed"
+        };
+        println!("端口 {}: {}", i, mark);
+    });
+}
