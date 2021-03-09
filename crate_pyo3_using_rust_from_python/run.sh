662c260b9b18e84564b48dcd1df9fb4beb639c0a
slyo <sean.lyo@outlook.com>
2021-03-09 20:31:11 +0800

    fix: Update pyo3 using rust from python example.

--- crate_pyo3_using_rust_from_python/run.sh
+++ crate_pyo3_using_rust_from_python/run.sh
@@ -1,3 +1,6 @@
-maturin develop && \
-python -c 'import rust2py; print(rust2py.__doc__); print(rust2py.sum_as_string(10, 20))'
-pip uninstall -y rust2py
+set -e
+
+cargo build --release
+cd target/release
+python -c 'import string_sum; print(string_sum.sum_as_string(1, 2))'
+cd ../../
--- crate_pyo3_using_rust_from_python/src/lib.rs
+++ crate_pyo3_using_rust_from_python/src/lib.rs
@@ -1,25 +1,16 @@
 use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
 
-// add bindings to the generated Python module
-// N.B: "rust2py" must be the name of the `.so` or `.pyd` file.
-
-/// This module is implemented in Rust.
-#[pymodule]
-fn rust2py(py: Python, m: &PyModule) -> PyResult<()> {
-    // PyO3 aware function. All of our Python interfaces could be declared in a separate module.
-    // Note that the `#[pyfn()]` annotation automatically converts the arguments from
-    // Python objects to Rust values, and the Rust return value back into a Python object.
-    // The `_py` argument represents that we're holding the GIL.
-    #[pyfn(m, "sum_as_string")]
-    fn sum_as_string_py(_py: Python, a: i64, b: i64) -> PyResult<String> {
-        let out = sum_as_string(a, b);
-        Ok(out)
-    }
-
-    Ok(())
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
 }
 
-// logic implemented as a normal Rust function
-fn sum_as_string(a: i64, b: i64) -> String {
-    format!("{}", a + b)
+/// A Python module implemented in Rust.
+#[pymodule]
+fn string_sum(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+
+    Ok(())
 }
