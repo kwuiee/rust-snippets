bb895d837daa11b752668541be3b65a02ede3d98
slyo <sean.lyo@outlook.com>
2021-10-14 08:36:40 +0800

    feat(doku): Generate doc from code.

---
+++ crate_doku_doc_from_code/src/main.rs
@@ -0,0 +1,29 @@
+use doku::Document;
+use serde::Deserialize;
+
+#[derive(Deserialize, Document)]
+struct Config {
+    /// Database's engine
+    db_engine: DbEngine,
+
+    /// Database's host
+    #[doku(example = "localhost")]
+    db_host: String,
+
+    /// Database's port
+    #[doku(example = "5432")]
+    db_port: usize,
+}
+
+#[derive(Deserialize, Document)]
+enum DbEngine {
+    #[serde(rename = "pgsql")]
+    PostgreSQL,
+
+    #[serde(rename = "mysql")]
+    MySQL,
+}
+
+fn main() {
+    println!("{}", doku::to_json::<Config>());
+}
