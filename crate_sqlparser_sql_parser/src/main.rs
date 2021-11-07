7e04372390d777de55d276acd865db99daa15c1d
slyo <sean.lyo@outlook.com>
2021-11-07 21:26:41 +0800

    feat(sqlparser): SQL lexer and parser.

---
+++ crate_sqlparser_sql_parser/src/main.rs
@@ -0,0 +1,15 @@
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser;
+
+fn main() {
+    let sql = "SELECT a, b, 123, myfunc(b) \
+           FROM table_1 \
+           WHERE a > b AND b < 100 \
+           ORDER BY a DESC, b";
+
+    let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
+
+    let ast = Parser::parse_sql(&dialect, sql).unwrap();
+
+    println!("AST: {:?}", ast);
+}
