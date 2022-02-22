5e60f632ea0aac15cb0045b0c629b7fad280697a
slyo <sean.lyo@outlook.com>
2022-02-22 23:36:20 +0800

    feat(action/buffer-io): Buffered io with file path tracing.

--- action_buffered_io_debug/debug.sh
+++ action_buffered_io_debug/debug.sh
@@ -6,3 +6,9 @@ strace --trace=write target/release/02_buffered
 strace --trace=read target/release/03_unbuffered_json
 # Trace syscall read, buffered
 strace --trace=read target/release/04_buffered_json
+
+# Trace syscall unbuffered read
+# The strace option --decode-fds=path tells us to which file the descriptor 3 refers (in this case, /tmp/simple.json)
+# which can be helpful to know what part of the program is at fault.
+strace --decode-fds=path --trace=read target/release/03_unbuffered_json 2>&1 | \
+	awk '/= 1$/'
