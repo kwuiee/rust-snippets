b10907d935c8afce6bc8f164a7856c16a4e36b6c
slyo <sean.lyo@outlook.com>
2022-04-20 13:48:43 +0800

    fix

--- crate_libwfa_wavefront_algorithm/src/main.rs
+++ crate_libwfa_wavefront_algorithm/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
         String::from("CTCCGGGCAGATAGCTGAGCATTTCATCATTGTAACTGGACTCCAGGCTGATGATCTCATCAATGACATCATCAATCATGTCACTTCCCATCATGGAACCACTCATGGTTGCTGGTGGAACGCCAGGATTAGCTTCATAACCTATGCCAC").into_bytes(),
         String::from("GGGTGATTATAGAGGCTTTGGCATAGGATCAGAAATTCTGAAGAATCTAAGCCAGGTTGCAATGAGGTGTCGCTGCAGCAGCATGCACTTCTTGGTAGCAGAATGGAATGTACCAGGACACCATGGGACCCTCCCAGCATGTCTATGCCT").into_bytes()
     ];
-    let pattern = concat!(include_str!("TFE3"), include_str!("SFPQ"))
+    let text = concat!(include_str!("TFE3"), include_str!("SFPQ"))
         .replace('\n', "")
         .into_bytes();
 
@@ -24,19 +24,16 @@ fn main() {
             gap_extension: 2,
         };
 
-        let pat_len = pattern.len();
-        let text_len = query.len();
-
         let mut wavefronts =
-            AffineWavefronts::new_complete(pat_len, text_len, &mut penalties, &alloc);
-        // let mut wavefronts = AffineWavefronts::new_reduced(pat_len, text_len, &mut penalties, 20, 10, &alloc);
+            AffineWavefronts::new_complete(query.len(), text.len(), &mut penalties, &alloc);
+        // let mut wavefronts = AffineWavefronts::new_reduced(query.len(), text.len(), &mut penalties, 20, 10, &alloc);
 
-        wavefronts.align(&pattern, &query).unwrap();
+        wavefronts.align(&query, &text).unwrap();
 
         let score = wavefronts.edit_cigar_score(&mut penalties);
         println!("score: {}", score);
 
-        // wavefronts.print_cigar(pattern, query);
+        // wavefronts.print_cigar(&query, &text);
 
         // Prettier byte vector
         let cigar = wavefronts.cigar_bytes();
@@ -46,31 +43,33 @@ fn main() {
         let cigar = wavefronts.cigar_bytes_raw();
         // println!("{}", std::str::from_utf8(&cigar).unwrap());
 
-        let mut pattern_padded: Vec<u8> = vec![];
-        let mut query_padded: Vec<u8> = vec![];
-        let mut query_iter = query.iter();
-        let mut pattern_iter = pattern.iter();
+        let mut padded_query: Vec<u8> = vec![];
+        let mut padded_text: Vec<u8> = vec![];
+        let mut textiter = text.iter();
+        let mut queryiter = query.iter();
         cigar.iter().for_each(|byte| match byte {
             b'D' => {
-                query_padded.push(b'-');
-                pattern_padded.push(*pattern_iter.next().unwrap())
+                padded_text.push(b'-');
+                padded_query.push(*queryiter.next().unwrap())
             }
             b'I' => {
-                pattern_padded.push(b'-');
-                query_padded.push(*query_iter.next().unwrap())
+                padded_query.push(b'-');
+                padded_text.push(*textiter.next().unwrap())
             }
             _ => {
-                query_padded.push(*query_iter.next().unwrap());
-                pattern_padded.push(*pattern_iter.next().unwrap())
+                // X: substitution
+                // M: match
+                padded_text.push(*textiter.next().unwrap());
+                padded_query.push(*queryiter.next().unwrap())
             }
         });
 
         unsafe {
             println!(
                 "{}\n{}\n{}",
-                std::str::from_utf8_unchecked(&pattern_padded),
+                std::str::from_utf8_unchecked(&padded_text),
                 std::str::from_utf8_unchecked(&cigar),
-                std::str::from_utf8_unchecked(&query_padded)
+                std::str::from_utf8_unchecked(&padded_query),
             );
         }
     }
